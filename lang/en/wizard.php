9cdf7c5e6d8cd71cbc10fd06cdacc763f9ee1ae1
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:54:42 +0100

    .

--- lang/en/wizard.php
+++ lang/en/wizard.php
@@ -4,17 +4,6 @@ declare(strict_types=1);
 
 return [
     'submit' => [
-<<<<<<< HEAD
-<<<<<<< HEAD
         'label' => 'Finish',
-=======
-
-        'label' => 'Finish',
-        'label' => 'Concludi',
-        'label' => 'Concludi',
->>>>>>> a14d070 (.)
-=======
-        'label' => 'Finish',
->>>>>>> cbfae69 (.)
     ],
 ];
