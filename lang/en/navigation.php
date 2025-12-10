4a746f51ca0b5f6b7022b8ab0aa8e981a3f5f286
marco76tv <marco.sottana@gmail.com>
2025-12-10 09:56:31 +0100

    .

--- lang/en/navigation.php
+++ lang/en/navigation.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 return [
     'main' => 'Main navigation',
-<<<<<<< HEAD
-    'site_title' => 'TechPlanner',
-=======
     'site_title' => '',
->>>>>>> d7ab3bb (.)
     'home' => 'Home',
     'about' => 'About',
     'contact' => 'Contact',
