db517dcc29db0d038b7cb0a7d2975e5653729c56
marco76tv <marco.sottana@gmail.com>
2025-12-10 09:56:29 +0100

    .

--- lang/en/navigation.php
+++ lang/en/navigation.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 return [
     'main' => 'Main navigation',
+<<<<<<< HEAD
     'site_title' => 'TechPlanner',
+=======
+    'site_title' => '',
+>>>>>>> d7ab3bb (.)
     'home' => 'Home',
     'about' => 'About',
     'contact' => 'Contact',
