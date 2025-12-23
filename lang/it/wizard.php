5396a4f9b16bbd2b8648ac55f58222d6c3b449fe
marco76tv <marco.sottana@gmail.com>
2025-12-23 13:57:29 +0100

    .

--- lang/it/wizard.php
+++ lang/it/wizard.php
@@ -1,8 +1,16 @@
 <?php
+<<<<<<< HEAD
+=======
+
+>>>>>>> d7ab3bb (.)
 declare(strict_types=1);
 
 return [
     'submit' => [
         'label' => 'Concludi',
     ],
+<<<<<<< HEAD
 ];
+=======
+];
+>>>>>>> d7ab3bb (.)
